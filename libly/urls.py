bc63abfdaaa8a68844c8ff01407e70ccb291474d
Sabina <sabinasidarovich@gmail.com>
2023-01-27 04:01:31 +0100

    minor fixes

--- libly/urls.py
+++ libly/urls.py
@@ -18,9 +18,10 @@ from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
+                  re_path(r'^web/', include('django.contrib.auth.urls')),
                   path('admin/', admin.site.urls),
                   path('', include('books.urls')),
                   path('', include('feed.urls')),
@@ -43,4 +44,3 @@ urlpatterns = [
                        name='password_reset_complete'),
               ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]) + static(settings.MEDIA_URL,
                                                                                                    document_root=settings.MEDIA_ROOT)
-
--- users/forms.py
+++ users/forms.py
@@ -6,11 +6,23 @@ from users.models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
+
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError("the given email is already registered")
+        return self.cleaned_data['email']
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 
 class UserUpdateForm(forms.ModelForm):
